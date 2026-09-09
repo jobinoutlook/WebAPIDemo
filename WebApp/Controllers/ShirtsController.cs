172e6aeedc1c476b3b2e7ef862bc33a220e02ce8
jobinoutlook <jobin.samuel@outlook.com>
2026-09-09 06:24:23 +0530

    Shirt V C updated

--- WebApp/Controllers/ShirtsController.cs
+++ WebApp/Controllers/ShirtsController.cs
@@ -1,13 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
 using WebApp.Models.Repositories;
 
 namespace WebApp.Controllers
 {
     public class ShirtsController : Controller
     {
-        public IActionResult Index()
+        private readonly IWebApiExecuter webApiExecuter;
+        public ShirtsController(IWebApiExecuter webApiExecuter)
         {
-            return View(ShirtRepository.GetShirts());
+            this.webApiExecuter = webApiExecuter;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await webApiExecuter.InvokeGet<List<Shirt>>("shirts"));
+        }
+
+        public IActionResult CreateShirt()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateShirt(Shirt shirt)
+        {
+            return View();
         }
     }
 }
---
+++ WebApp/Data/WebApiExecuter.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Data
+{
+    public class WebApiExecuter : IWebApiExecuter
+    {
+        private const string apiName = "ShirtsApi";
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public WebApiExecuter(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T?> InvokeGet<T>(string relativeUrl)
+        {
+            var httpClient = _httpClientFactory.CreateClient(apiName);
+            return await httpClient.GetFromJsonAsync<T>(relativeUrl);
+        }
+
+    }
+}
